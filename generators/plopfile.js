d845c403d85b8896ba52f5f33ae20ca10e37fe0a
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-06 09:57:31 -0300

    Generic Test Generator

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -4,7 +4,7 @@ module.exports = function (plop) {
     prompts: [
       { type: "input", name: "name", message: "What is the name of the entity?" },
     ],
-    actions: [...entitiesCreations]
+    actions: [...entitiesCreations],
   });
   plop.setGenerator("useCases", {
     description: "Create a new useCase",
@@ -13,8 +13,34 @@ module.exports = function (plop) {
     ],
     actions: [...useCasesCreations],
   });
+  plop.setGenerator("test", {
+    description: "Create a new test",
+    prompts: [
+      { type: "input", name: "name", message: "What is the name of the file?" },
+      { type: "input", name: "entity", message: "What is the name of the entity?" },
+      { type: "input", name: "layer", message: "What is the name of the layer?" },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../src/slices/{{camelCase entity}}/{{camelCase layer}}/{{pascalCase name}}.spec.ts",
+        templateFile: "./templates/test.spec.ts.hbs",
+      },
+    ],
+  });
 };
-
+const useCasesCreations = [
+  {
+    type: "add",
+    path: "../src/slices/{{camelCase name}}/repositories/contracts/index.ts",
+    templateFile: "./templates/repositories/contracts/index.ts.hbs",
+  },
+  {
+    type: "add",
+    path: "../src/slices/{{camelCase name}}/repositories/contracts/add{{pascalCase name}}Repository.ts",
+    templateFile: "./templates/repositories/contracts/addDomainRepository.ts.hbs",
+  },
+];
 const entitiesCreations = [
   {
     type: "add",
@@ -30,18 +56,5 @@ const entitiesCreations = [
     type: "add",
     path: "../src/slices/{{camelCase name}}/entities/index.ts",
     templateFile: "./templates/entities/index.ts.hbs",
-  },
-];
-
-const useCasesCreations = [
-  {
-    type: "add",
-    path: "../src/slices/{{camelCase name}}/repositories/contracts/index.ts",
-    templateFile: "./templates/repositories/contracts/index.ts.hbs",
-  },
-  {
-    type: "add",
-    path: "../src/slices/{{camelCase name}}/repositories/contracts/add{{pascalCase name}}Repository.ts",
-    templateFile: "./templates/repositories/contracts/addDomainRepository.ts.hbs",
   },
 ];
